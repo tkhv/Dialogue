24b855a452d01a9dcb2dd7e49b73cda0870f070d
Thottempudi, Harsha <harsha.02k@gmail.com>
2024-03-26 13:43:21 -0400

    Revamp auth components, + navbar

--- app/auth/signup/page.tsx
+++ app/auth/signup/page.tsx
@@ -1,7 +1,56 @@
+"use client";
+
+import Papa from "papaparse";
+
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
 import Link from "next/link";
-import { SignupForm } from "../components/signupForm";
 
 export default function LoginPage() {
+  function submit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log(data.get("email"));
+    console.log(data.get("password"));
+
+    // Get the file from the form data
+    const file = data.get("file") as File | null;
+
+    // Print the file
+    if (file) {
+      console.log(`File name: ${file.name}`);
+      console.log(`File type: ${file.type}`);
+      console.log(`File size: ${file.size} bytes`);
+
+      // Check if the file type is CSV
+      if (file.type === "text/csv") {
+        // Parse the CSV file
+        Papa.parse(file, {
+          complete: function (results) {
+            console.log("Parsed results:", results.data);
+          },
+          error: function (err) {
+            console.log("Error parsing CSV:", err);
+          },
+        });
+      } else {
+        console.log("Invalid file type. Please upload a CSV file.");
+      }
+    } else {
+      console.log("No file selected");
+    }
+    return;
+  }
+
   return (
     <div
       style={{
@@ -13,7 +62,63 @@ export default function LoginPage() {
         color: "white",
       }}
     >
-      <SignupForm />
+      <Card className="mx-auto max-w-sm">
+        <CardHeader>
+          <CardTitle className="text-xl">Sign Up</CardTitle>
+          <CardDescription>
+            Optionally, import ratings from Letterboxd and IMDb to improve club
+            reccomendations.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form onSubmit={submit}>
+            <div className="grid gap-4">
+              <div className="grid grid-cols-2 gap-4">
+                <div className="grid gap-2">
+                  <Label htmlFor="first-name">First name</Label>
+                  <Input
+                    id="first-name"
+                    name="first-name"
+                    placeholder="John"
+                    required
+                  />
+                </div>
+                <div className="grid gap-2">
+                  <Label htmlFor="last-name">Last name</Label>
+                  <Input
+                    id="last-name"
+                    name="last-name"
+                    placeholder="Doe"
+                    required
+                  />
+                </div>
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  type="email"
+                  name="email"
+                  placeholder="john@example.com"
+                  required
+                />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Password</Label>
+                <Input id="password" name="password" type="password" required />
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="password">Import Ratings</Label>
+                <Input type="file" name="file" accept=".csv" />
+              </div>
+              <Button type="submit" className="w-full">
+                Create an account
+              </Button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+
       <div className="mt-4 text-center text-sm">
         Already have an account?{" "}
         <Link href="/auth/signin" className="underline">
