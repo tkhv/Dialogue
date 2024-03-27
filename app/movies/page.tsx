6576a15ad4afae76ec64240d0fb3e16ee6a614a6
Thottempudi, Harsha <harsha.02k@gmail.com>
2024-03-27 10:59:40 -0400

    Moved navbar component

---
+++ app/movies/page.tsx
@@ -0,0 +1,16 @@
+export default function HomePage() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        flex: 1,
+        color: "black",
+      }}
+    >
+      Movies
+    </div>
+  );
+}
